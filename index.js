41b3eb1395989215f83ce0afe40b14bc537eb6e6
shinto <shintopa@outlook.com>
2023-12-06 13:47:38 +0530

    mongoose

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+//const dbConnect = require('./config/dbConnection');
 const dotenv = require("dotenv").config();
+const register = require('./routes/authRoutes')
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
-app.listen(PORT,()=>{
-    console.log('Server successfull run port '+PORT);
+
+mongoose.connect(process.env.ONGO_DB_URL).then((data) => { console.log(data) }).catch((e) => { console.log(e) })
+//dbConnect();
+
+app.use('/', (req, res) => {
+    res.send("Your ecommerce project started");
+});
+app.use('user/use',register);
+app.listen(PORT, () => {
+    console.log('Server successfull run port ' + PORT);
 });
---
+++ routes/authRoutes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const routes = express.Router();
+
+routes.post("/register");
+
+module.exports = routes;
